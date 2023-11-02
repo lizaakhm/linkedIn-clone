0b6e70e91007e00024092925a84013458ffdb648
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2023-11-02 19:28:57 +0400

    cleaned commented codes

--- src/Components/NewsFeed.jsx
+++ src/Components/NewsFeed.jsx
@@ -3,8 +3,7 @@ import follobtn from "./followbutton.svg";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import CloseIcon from "@mui/icons-material/Close";
 import "./NewsFeed.css";
-import feed_img from './feedimg.svg'
-import chatsign from './chatsign.svg'
+import chatsign from "./chatsign.svg";
 
 const NewsFeed = ({ name, description, message, avatar }) => {
   return (
@@ -13,12 +12,16 @@ const NewsFeed = ({ name, description, message, avatar }) => {
         <Avatar style={{ width: "63px", height: "63px" }} />
         <div className="feed_info">
           <h2>Anthony J Jackson</h2>
-          <p className="user_prof">CEO, Innovation, Technology & Growth at Trinity Consulting</p>
+          <p className="user_prof">
+            CEO, Innovation, Technology & Growth at Trinity Consulting
+          </p>
         </div>
         <div className="icons">
           <div className="feed_icons">
-            <MoreVertIcon style={{ fill: "#25314C", fontSize: "medium", cursor:'pointer' }} />
-            <CloseIcon style={{ fontSize: "medium", cursor:'pointer' }} />
+            <MoreVertIcon
+              style={{ fill: "#25314C", fontSize: "medium", cursor: "pointer" }}
+            />
+            <CloseIcon style={{ fontSize: "medium", cursor: "pointer" }} />
           </div>
           <img
             src={follobtn}
@@ -28,21 +31,19 @@ const NewsFeed = ({ name, description, message, avatar }) => {
         </div>
       </div>
       <p className="status_text">hihihihihihihihihihi</p>
-      {/* <div className = "nino"> */}
-      
-      <div className="post_img">
-        {/* <img src={feed_img} alt="" /> */}
-      </div>
-      {/* </div> */}
+      <div className="post_img"></div>
       <div className="comment">80 Comments</div>
       <div className="line"></div>
       <div className="comments">
-        <img src={chatsign} alt="chatsign" style={{width: '14px', height:'14px'}}/>
+        <img
+          src={chatsign}
+          alt="chatsign"
+          style={{ width: "14px", height: "14px" }}
+        />
         <p className="comment_p">Comment</p>
       </div>
     </div>
   );
 };
-
 
 export default NewsFeed;
